cfdff65f6badcf4e7afaad4e6852076ba7ac215d
Maria Margarida <mmargarida688rgmail.com>
2025-02-21 18:00:56 +0000

    Added project Smiley and tried to create a small program.

---
+++ Smiley/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        Console.WriteLine("Insire código: ");
+            string numero_inteiro = Console.ReadLine();
+            int numero = int.Parse(numero_inteiro);
+
+            string numero_hexadecimal = Convert.ToString("x");
+            char numero_unicode = Convert.ToChar(numero);
+
+        Console.WriteLine("Caráter Unicode: " + numero_unicode);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
