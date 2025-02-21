1b6fec0c36d99b3f0fce953cdbdd1f1a9e182e96
Maria Margarida <mmargarida688rgmail.com>
2025-02-21 13:16:06 +0000

    Created new project VariousTypes and modified LP1Semana01.sln.

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            sbyte numero1 = -100;
+            short numero2 = 2;
+            int numero3 = 30;
+            long numero4 = 40000;
+            byte numero5 = 200;
+            ushort numero6 = 50000;
+            uint numero7 = 4000000000U;
+            ulong numero8 = 6000000000000000UL;
+            Console.WriteLine("sbyte: " + numero1);
+            Console.WriteLine("short: " + numero2);
+            Console.WriteLine("int: " + numero3);
+            Console.WriteLine("long: " + numero4);
+            Console.WriteLine("byte: " + numero5);
+            Console.WriteLine("ushort: " + numero6);
+            Console.WriteLine("uint: " + numero7);
+            Console.WriteLine("ulong: " + numero8);
+        }
+    }
+}
